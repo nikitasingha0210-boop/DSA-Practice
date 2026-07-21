1c2df1f36095f7ecfcdd47f8aa9dc0ccb88f1ac1
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-22 00:20:49 +0530

    Ad LeerCode 27-Remove Element

---
+++ Arrays/Remove Element.cpp
@@ -0,0 +1,24 @@
+/*
+LeetCode 27
+problem : Remove Element
+Topic : Two pointers
+Difficulty: Easy
+Language : C++
+
+*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != val){
+                nums[k] = nums [i];
+                k++ ;
+            }
+        }
+        return k;
+        
+    }
+};
