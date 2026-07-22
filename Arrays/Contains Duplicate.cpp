686472413eeb50bd47fd908f5f6bb0fb216574ab
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-22 11:33:14 +0530

    Add LeetCode 217 - Contains Dupliacte

---
+++ Arrays/Contains Duplicate.cpp
@@ -0,0 +1,22 @@
+/*
+LeetCode 217
+Problem: Contains Duplicate
+Topic: Hashing
+Difficulty: Easy
+Language: C++
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+            sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+
+        return false;
+        
+    }
+};
