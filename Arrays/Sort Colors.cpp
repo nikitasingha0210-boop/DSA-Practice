66d4a1aabb6a66b45b5bb1d0b454276e42d938c2
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-24 12:27:42 +0530

    Add LeetCode 75 - Sort colors

---
+++ Arrays/Sort Colors.cpp
@@ -0,0 +1,16 @@
+/*
+LeetCode 75
+Problem: Sort Colors
+Topic: Two Pointers (Dutch National Flag Algorithm)
+Difficulty: Medium
+*/
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+    }
+};
