02654233b8ce223c83aee64b4bdc6c3fdcc42c93
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-26 15:43:35 +0530

    Add LeetCode 58 - Length of Last Word

---
+++ Arrays/Length of Last Word.cpp
@@ -0,0 +1,28 @@
+/*
+LeetCode 58
+Problem: Length of Last Word
+Topic: Strings
+Difficulty: Easy
+Language: C++
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // Count the last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
