c1f759c2b9eaa53797fdf3a4d706401bbacedfbc
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-26 15:35:49 +0530

    Add LeetCode 628 - Maximum Product of Three Numbers

---
+++ Arrays/Maximum Product of Three Numbers.cpp
@@ -0,0 +1,18 @@
+/*
+LeetCode 628
+Problem: Maximum Product of Three Numbers
+Topic: Arrays
+Difficulty: Easy
+Language: C++
+*/
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        return max(nums[0] * nums[1] * nums[n - 1],
+                   nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
+};
