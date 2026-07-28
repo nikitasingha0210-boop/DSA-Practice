98b90c69a5fed3dc9cd75f1904c359189575005e
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-28 22:52:48 +0530

    Add LeetCode 3517 - Smallest Plindromic Rearrangement

---
+++ Arrays/Smallest Palindromic Rearrangement.cpp
@@ -0,0 +1,33 @@
+/*
+LeetCode 3517
+Problem: Smallest Palindromic Rearrangement
+Topic: Strings
+Difficulty: Medium
+Language: C++
+*/
+
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        string left = "";
+        string mid = "";
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] % 2 == 1)
+                mid = char(i + 'a');
+
+            left.append(freq[i] / 2, char(i + 'a'));
+        }
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        return left + mid + right;
+    }
+};
+
