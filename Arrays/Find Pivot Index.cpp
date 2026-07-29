cdf5714db13a4393c9e367a6fe17087c8bf4b111
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-29 23:20:53 +0530

    Add LeetCode 724 - Find Pivot Index

---
+++ Arrays/Find Pivot Index.cpp
@@ -0,0 +1,30 @@
+/*
+LeetCode 724
+Problem: Find Pivot Index
+Topic: Arrays
+Difficulty: Easy
+Language: C++
+*/
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+
+        for (int num : nums)
+            totalSum += num;
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum)
+                return i;
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
