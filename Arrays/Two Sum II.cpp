3f1b241a2d45862e8d5a26affb4edfbcfd843f4c
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-29 22:57:51 +0530

    Add LeetCode 167 - Two Sum II

---
+++ Arrays/Two Sum II.cpp
@@ -0,0 +1,31 @@
+/*
+LeetCode 167
+Problem: Two Sum II - Input Array Is Sorted
+Topic: Arrays
+Difficulty: Medium
+Language: C++
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return {left + 1, right + 1}; // 1-based indexing
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return {};
+    }
+};
