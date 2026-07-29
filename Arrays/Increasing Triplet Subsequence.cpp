4b0e9e132f10da373d69dac69d119d30eb98b35f
Nikita Singha <nikitasingha0210@gmail.com>
2026-07-29 23:06:08 +0530

    Add LeetCode 334 - Increasing Triplet Subsequece

---
+++ Arrays/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,29 @@
+/*
+LeetCode 334
+Problem: Increasing Triplet Subsequence
+Topic: Arrays
+Difficulty: Medium
+Language: C++
+*/
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            }
+            else if (num <= second) {
+                second = num;
+            }
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
